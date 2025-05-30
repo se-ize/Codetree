202b91e87bce6dbaf2662f7e8a96da5156caa239
se-ize <70623290+se-ize@users.noreply.github.com>
2025-05-30 20:18:49 +0900

    Title: Date to Date, Time: 117ms, Memory: 10MB, Status: Passed - Codetree

--- 250530/Date to Date/date-to-date.java
+++ 250530/Date to Date/date-to-date.java
@@ -9,35 +9,19 @@ public class Main {
         // Please write your code here.
         int all = 0;
         int sub = 0;
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-        while (m2 > 1) {
-            if (m2 == 2) {
-                all += 28;
-            } else {
-                all += 31;
-            }
-            m2--;
+        for (int i=0; i<m2-1; i++) {
+            all += days[i];
         }
-
         all += d2;
 
-        while (m1 > 1) {
-            if (m1 == 2) {
-                sub += 28;
-            } else {
-                sub += 31;
-            }
-            m1--;
+        for (int j=0; j<m1-1; j++) {
+            sub += days[j];
         }
-
         sub += d1;
 
-        if (m1 == m2 && d1 == d2) {
-            all = 1;
-            sub = 0;
-        }
-
-        System.out.print(all-sub);
+        System.out.print(all-sub+1);
         
     }
 }
