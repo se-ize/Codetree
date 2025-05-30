af334990d4d78ba37561ceabc025c579952dde7e
se-ize <70623290+se-ize@users.noreply.github.com>
2025-05-30 19:35:04 +0900

    Title: Date to Date, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250530/Date to Date/date-to-date.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        // Please write your code here.
+        int all = 0;
+        int sub = 0;
+
+        while (m2 > 0) {
+            if (m2 == 2) {
+                all += 28;
+            } else {
+                all += 31;
+            }
+            m2--;
+        }
+
+        all += d2;
+
+        while (m1 > 0) {
+            if (m1 == 2) {
+                sub += 28;
+            } else {
+                sub += 31;
+            }
+            m1--;
+        }
+
+        sub += d1;
+
+        System.out.print(all-sub);
+        
+    }
+}
