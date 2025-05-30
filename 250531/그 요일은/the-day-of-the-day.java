f8a0620e5e3021a3a0d862c9f8dc173ea1c3ebfc
se-ize <70623290+se-ize@users.noreply.github.com>
2025-05-31 00:25:06 +0900

    Title: 그 요일은, Time: 133ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250531/그 요일은/the-day-of-the-day.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        String A = sc.next();
+        // Please write your code here.
+
+        int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        String[] days = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+        int one = 0;
+        int two = 0;
+
+        for (int i = 0; i < m1-1; i++) {
+            one += date[i];
+        }
+        one += d1;
+
+        for (int i = 0; i < m2-1; i++) {
+            two += date[i];
+        }
+        two += d2;
+
+        int index = 0;
+        int answer = (two - one) % 7;
+
+        for (int i = 0; i < days.length; i++) {
+            if (days[i].equals(A)) index = i;
+        }
+
+        if (index < answer) answer++;
+
+        System.out.print(answer);
+
+    }
+}
