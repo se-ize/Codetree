2693ebae3cd5c0f770e004c335dfacb840db7724
se-ize <70623290+se-ize@users.noreply.github.com>
2025-06-01 04:35:47 +0900

    Title: 십진수와 이진수 2, Time: 116ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250601/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String binary = sc.next();
+        // Please write your code here.
+        int decimal = 0;
+        int decimal17 = 0;
+
+        for (int i=0; i<binary.length(); i++) {
+            decimal = decimal * 2 + binary.charAt(i) - '0';
+        }
+        decimal17 = decimal * 17;
+
+        int cnt = 0;
+        int[] digits = new int[binary.length()*2];
+
+        while (true) {
+            if (decimal17 < 2) {
+                digits[cnt++] = decimal17;
+                break;
+            }
+            digits[cnt++] = decimal17 % 2;
+            decimal17 /= 2;
+        }
+        
+        for (int i=cnt-1; i>=0; i--) {
+            System.out.print(digits[i]);
+        }
+
+    }
+}
