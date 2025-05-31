5013ad1adffc7337527e7de41c14f7332a016622
se-ize <70623290+se-ize@users.noreply.github.com>
2025-06-01 04:25:38 +0900

    Title: 여러가지 진수변환, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250601/여러가지 진수변환/various-numeral-system-transformations.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int b = sc.nextInt();
+        // Please write your code here.
+        int[] digits = new int[n];
+        int cnt = 0;
+
+        while (true) {
+            if (b == 4) {
+                if (n < 4) {
+                    digits[cnt++] = n;
+                    break;
+                }
+                digits[cnt++] = n % 4;
+                n /= 4;
+            }
+            if (b == 8) {
+                if (n < 8) {
+                    digits[cnt++] = n;
+                    break;
+                }
+                digits[cnt++] = n % 8;
+                n /= 8;
+            }
+        }
+        
+        for (int i=cnt-1; i>=0; i--) {
+            System.out.print(digits[i]);
+        }
+    }
+}
