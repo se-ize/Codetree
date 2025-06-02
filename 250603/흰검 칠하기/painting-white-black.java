f1e8644028a111868f287534d84a3256bf1d421a
se-ize <70623290+se-ize@users.noreply.github.com>
2025-06-03 03:03:21 +0900

    Title: 흰검 칠하기, Time: 145ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250603/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        char[] lines = new char[2000];
+        int[] whites = new int[2000];
+        int[] blacks = new int[2000];
+        int idx = 1000;
+        int white = 0, black = 0, gray = 0;
+        
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char d = sc.next().charAt(0);
+            if (d == 'R') {
+                for (int j = idx; j < idx + x; j++) {
+                    if (lines[j] == 'G') continue;
+                    blacks[j]++;
+
+                    if (whites[j] >= 2 && blacks[j] >= 2) {
+                        lines[j] = 'G';
+                    } else lines[j] = 'B';
+                }
+                idx += (x - 1);
+            } else {
+                for (int j = idx; j > idx - x; j--) {
+                    if (lines[j] == 'G') continue;
+                    whites[j]++;
+
+                    if (whites[j] >= 2 && blacks[j] >= 2) {
+                        lines[j] = 'G';
+                    } else lines[j] = 'W';
+                }
+                idx -= (x - 1);
+            }
+        }
+
+        for (int i = 0; i < 2000; i++) {
+            if (lines[i] == 'W') white++;
+            else if (lines[i] == 'B') black++;
+            else if (lines[i] == 'G') gray++;
+        }
+        System.out.print(white + " " + black + " " + gray);
+    }
+}
