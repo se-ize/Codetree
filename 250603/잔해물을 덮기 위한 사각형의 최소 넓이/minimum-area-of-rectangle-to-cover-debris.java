a701d7f033e87a90021d4ac6ce91eeba258f7b3e
se-ize <70623290+se-ize@users.noreply.github.com>
2025-06-03 23:43:22 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 169ms, Memory: 26MB, Status: Wrong Answer - Codetree

---
+++ 250603/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rect1_x1 = sc.nextInt();
+        int rect1_y1 = sc.nextInt();
+        int rect1_x2 = sc.nextInt();
+        int rect1_y2 = sc.nextInt();
+        int rect2_x1 = sc.nextInt();
+        int rect2_y1 = sc.nextInt();
+        int rect2_x2 = sc.nextInt();
+        int rect2_y2 = sc.nextInt();
+        int offset = 1000;
+        int[][] area = new int[2000][2000];
+
+        for (int i = rect1_x1; i < rect1_x2; i++) {
+            for (int j = rect1_y1; j < rect1_y2; j++) {
+                area[i+offset][j+offset] = 1;
+            }
+        }
+
+        // 값이 1이라면 2로 설정 -> 1로 설정된 부분 최소/최대 행, 열 좌표 구하기
+        for (int i = rect2_x1; i < rect2_x2; i++) {
+            for (int j = rect2_y1; j < rect2_y2; j++) {
+                if (area[i+offset][j+offset] == 1) {
+                    area[i+offset][j+offset] = 2;
+                }
+            }
+        }
+
+        int x1 = 2000, y1 = 2000; //최소 행,열
+        int x2 = 0, y2 = 0; //최대 행,열
+        for (int i = 0; i < 2000; i++) {
+            for (int j = 0; j < 2000; j++) {
+                if (area[i][j] == 1) {
+                    if (i < x1) x1 = i;
+                    else if (i > x2) x2 = i;
+
+                    if (j < y1) y1 = j;
+                    else if (j > y2) y2 = j;
+                }
+            }
+        }
+        System.out.print(((x2+1)-x1)*((y2+1)-y1));
+    }
+}
