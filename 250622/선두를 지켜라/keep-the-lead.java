fe4b99cdc5a432c275a736ed93da67d1e8693af0
se-ize <70623290+se-ize@users.noreply.github.com>
2025-06-22 04:00:57 +0900

    Title: 선두를 지켜라, Time: 133ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250622/선두를 지켜라/keep-the-lead.java
+++ 250622/선두를 지켜라/keep-the-lead.java
@@ -21,26 +21,38 @@ public class Main {
         int idx = 0;
 
         for (int i = 0; i < n; i++) {
-            for (int j = idx; j < A[i][1]+idx; j++) {
-                raceA[j] = A[i][0];
+            for (int j = 0; j < A[i][1]; j++) {
+                raceA[idx + 1] = raceA[idx] + A[i][0]; //누적 거리 저장
+                idx++;
             }
-            idx = A[i][0];
         }
+
         idx = 0;
         for (int i = 0; i < m; i++) {
-            for (int j = idx; j < B[i][1]+idx; j++) {
-                raceB[j] = B[i][0];
+            for (int j = 0; j < B[i][1]; j++) {
+                raceB[idx + 1] = raceB[idx] + B[i][0];
+                idx++;
             }
-            idx = B[i][0];
         }
         
         char[] winners = new char[1001];
+        char prev = 'T'; //초기값
         int answer = 0;
-        for (int i = 0; i < 1000; i++) {
+        
+        for (int i = 1; i < 1001; i++) {
+            if (raceA[i] == 0 && raceB[i] == 0) break; // 둘 다 경주 끝났으면 종료
+
             if (raceA[i] > raceB[i]) winners[i] = 'A';
             else if (raceA[i] < raceB[i]) winners[i] = 'B';
-            
-            if (i > 0 && winners[i] != winners[i-1]) answer++;
+            else winners[i] = 'T';
+
+            if (winners[i] == 'T') continue; // 동점이면 prev 유지 (선두 유지 중)
+    
+            // 선두가 바뀐 경우에만 체크
+            if (winners[i] != prev) {
+                answer++;
+                prev = winners[i];
+            }
         }
         System.out.print(answer);
     }
