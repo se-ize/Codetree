54a1154989b53b6b7960e212c234f4fdbe9e12e7
se-ize <70623290+se-ize@users.noreply.github.com>
2025-06-22 23:50:31 +0900

    Title: 악수와 전염병의 상관관계 2, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250622/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 250622/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -7,7 +7,6 @@ public class Main {
         int P = sc.nextInt(); // 처음 감염돼있는 개발자
         int T = sc.nextInt();
         int[][] shakes = new int[T][3];
-        int[][] answer = new int[N][2];
         for (int i = 0; i < T; i++) {
             shakes[i][0] = sc.nextInt(); //t
             shakes[i][1] = sc.nextInt(); //x
@@ -15,6 +14,7 @@ public class Main {
         }
 
        // 처음 감염된 개발자 설정
+       int[][] answer = new int[N][2];
        answer[P-1][0] = 1; 
        // 전염 가능 횟수 초기화
        for (int i = 0; i < N; i++) {
@@ -24,21 +24,38 @@ public class Main {
        // 시간순으로 악수 기록 정렬
        Arrays.sort(shakes, (a, b) -> Integer.compare(a[0], b[0]));
 
-       for (int i = 0; i < T; i++) {
-        int x = shakes[i][1] - 1;
-        int y = shakes[i][2] - 1;
-        
-        // x가 감염자이고 전염 횟수 남아있으면 y 감염 시도
-        if (answer[x][0] == 1 && answer[x][1] > 0) {
+       int i = 0;
+while (i < T) {
+    int currentTime = shakes[i][0];
+    int start = i;
+
+    // 같은 시간대의 끝 인덱스 찾기
+    while (i < T && shakes[i][0] == currentTime) {
+        i++;
+    }
+
+    // 현재 감염 상태 복사
+    int[] infected = new int[N];
+    for (int j = 0; j < N; j++) {
+        infected[j] = answer[j][0];
+    }
+
+    // start부터 i-1까지 같은 시간대의 악수 처리
+    for (int j = start; j < i; j++) {
+        int x = shakes[j][1] - 1;
+        int y = shakes[j][2] - 1;
+
+        if (infected[x] == 1 && answer[x][1] > 0) {
             if (answer[y][0] == 0) answer[y][0] = 1;
             answer[x][1]--;
-        } 
-        // y가 감염자이고 전염 횟수 남아있으면 x 감염 시도
-        if (answer[y][0] == 1 && answer[y][1] > 0) {
+        }
+        if (infected[y] == 1 && answer[y][1] > 0) {
             if (answer[x][0] == 0) answer[x][0] = 1;
             answer[y][1]--;
-        } 
-       }
+        }
+    }
+}
+
 
        for (int i = 0; i < N; i++) {
         System.out.print(answer[i][0]); // 0-음성, 1-양성
