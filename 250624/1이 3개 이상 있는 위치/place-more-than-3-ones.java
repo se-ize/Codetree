7151085ba13870d5f68b12617b54b50a90ab5cff
se-ize <70623290+se-ize@users.noreply.github.com>
2025-06-24 16:13:10 +0900

    Title: 1이 3개 이상 있는 위치, Time: 192ms, Memory: 13MB, Status: Wrong Answer - Codetree

---
+++ 250624/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static boolean inRange(int x, int y, int N) {
+        return (0 <= x && x < N && 0 <= y && y < N);
+    }
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] array = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                array[i][j] = sc.nextInt();
+            }
+        }
+
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+        int cnt = 0, answer = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int dirNum = 0; dirNum < 4; dirNum++) {
+                    int nx = i + dx[dirNum];
+                    int ny = j + dy[dirNum];
+                    if (inRange(nx, ny, N) && array[nx][ny] == 1) cnt++;
+                }
+            }
+            if (cnt > 4) answer++;
+        }
+        System.out.print(answer);
+    }
+}
