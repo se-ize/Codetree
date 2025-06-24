6c4a000dddeab3bc4a4934201f3157884942d02b
se-ize <70623290+se-ize@users.noreply.github.com>
2025-06-24 16:19:35 +0900

    Title: 1이 3개 이상 있는 위치, Time: 229ms, Memory: 18MB, Status: Passed - Codetree

--- 250624/1이 3개 이상 있는 위치/place-more-than-3-ones.java
+++ 250624/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -17,16 +17,17 @@ public class Main {
 
         int[] dx = new int[]{0, 1, 0, -1};
         int[] dy = new int[]{1, 0, -1, 0};
-        int cnt = 0, answer = 0;
+        int answer = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
+                int cnt = 0;
                 for (int dirNum = 0; dirNum < 4; dirNum++) {
                     int nx = i + dx[dirNum];
                     int ny = j + dy[dirNum];
                     if (inRange(nx, ny, N) && array[nx][ny] == 1) cnt++;
                 }
+                if (cnt >= 3) answer++;
             }
-            if (cnt > 4) answer++;
         }
         System.out.print(answer);
     }
