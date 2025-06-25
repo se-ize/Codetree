5a5683fd40b80df995f3a4ae1bd1000b33d6a660
se-ize <70623290+se-ize@users.noreply.github.com>
2025-06-25 23:55:59 +0900

    Title: 되돌아오기 2, Time: 114ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250625/되돌아오기 2/come-back-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String commands = sc.next();
+        // Please write your code here.
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+        int x = 0, y = 0;
+        int dir = 3;
+        int time = 0;
+        boolean isReturned = false;
+
+        for (int i = 0; i < commands.length(); i++) {
+            char c = commands.charAt(0);
+            if (c == 'L') {
+                dir = (dir + 3) % 4;
+                time++;
+            } else if (c == 'R') {
+                dir = (dir + 1) % 4;
+                time++;
+            } else {
+                x += dx[dir];
+                y += dy[dir];
+                time++;
+            }
+
+            if (x == 0 && y == 0) {
+                isReturned = true;
+                break;
+            }
+        }
+        System.out.print(isReturned ? time : -1);
+    }
+}
