ed25f6ed75666d3e7ed686988a4b0fa28f816ebe
se-ize <70623290+se-ize@users.noreply.github.com>
2025-07-17 15:06:44 +0900

    Title: 수 등장 횟수, Time: 1433ms, Memory: 47MB, Status: Passed - Codetree

--- 250717/수 등장 횟수/number-frequency.java
+++ 250717/수 등장 횟수/number-frequency.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Main {
     public static HashMap<Integer, Integer> hashMap = new HashMap<>();
+    // key - 수열의 원소, value - 등장 횟수
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -8,7 +9,7 @@ public class Main {
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-            hashMap.put(arr[i], i + 1); //1-based index
+            hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0) + 1);
         }
         int[] queries = new int[m];
         for (int i = 0; i < m; i++) {
