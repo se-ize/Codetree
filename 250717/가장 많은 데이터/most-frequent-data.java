07fd476744559723e9ea14121b7ed842b906916c
se-ize <70623290+se-ize@users.noreply.github.com>
2025-07-17 15:28:04 +0900

    Title: 가장 많은 데이터, Time: 708ms, Memory: 42MB, Status: Passed - Codetree

--- 250717/가장 많은 데이터/most-frequent-data.java
+++ 250717/가장 많은 데이터/most-frequent-data.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int maxCount = 0;
         String[] arr = new String[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.next();
             hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0) + 1);
+            maxCount = Math.max(maxCount, hashMap.get(arr[i]));
         }
 
-        int maxCount = 0;
-
-        for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
-            if (entry.getValue() > maxCount) maxCount = entry.getValue();
-        }
+        // for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+        //     if (entry.getValue() > maxCount) maxCount = entry.getValue();
+        // }
 
         System.out.print(maxCount);
         // Please write your code here.
