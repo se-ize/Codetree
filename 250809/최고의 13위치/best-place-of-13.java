8c6a24d5b18d98ee7fb9afba0415c5d612610839
se-ize <70623290+se-ize@users.noreply.github.com>
2025-08-09 21:08:23 +0900

    Title: 최고의 13위치, Time: 163ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 250809/최고의 13위치/best-place-of-13.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] grid = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+        // Please write your code here.
+        int maxCnt = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n - 2; j++) 
+                maxCnt = Math.max(maxCnt, grid[i][j] + grid[i][j+1] + grid[i][j+2]);
+
+        System.out.println(maxCnt);
+    }
+}
