39015a8affa7045d664764f0436af8c6f8aed39b
se-ize <70623290+se-ize@users.noreply.github.com>
2025-08-12 17:43:05 +0900

    Title: 구간 중 최대 합, Time: 158ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250812/구간 중 최대 합/max-sum-of-subarray.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        // Please write your code here.
+        int max = Integer.MIN_VALUE;
+        
+        for (int i = 0; i <= n - k; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += arr[j];
+            }
+            max = Math.max(max, sum);
+        }
+        System.out.print(max);
+    }
+}
